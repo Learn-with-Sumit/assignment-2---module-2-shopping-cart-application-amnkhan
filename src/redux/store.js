15113322caf02efb712e2885f691453686f5c2f5
Al Amin Khan <amnkhan.me@gmail.com>
2022-09-01 03:58:27 +0600

    Added Stock Checker Middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,21 @@
 import addToCartReducer from "./AddToCart/addToCartReducer";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
-const store = createStore(addToCartReducer);
+// Stockchecker middleware
+const checkStock = (store) => (next) => (action) => {
+  const upcomingStock = [action].reduce(addToCartReducer, store.getState());
+
+  // check if the stock is available for the action dispatched
+  upcomingStock.forEach((product) => {
+    if (action.payload.productId === product.id) {
+      if (product.stock > -1) {
+        return next(action);
+      }
+    }
+  });
+};
+
+// Redux store
+const store = createStore(addToCartReducer, applyMiddleware(checkStock));
 
 export default store;
